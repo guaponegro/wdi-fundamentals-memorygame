eff5e33971f330773a29d4279da438280534d0f4
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-09-08 22:26:47 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,31 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+	seeCardImage: function () {
+	console.log(cards.cardImage);
+	console.log(cards.suit);
+}	
+}
+];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function()
@@ -11,8 +38,8 @@ console.log("Sorry, try again.");
 };
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 };
 	flipCard(0);
@@ -22,7 +49,7 @@ var flipCard = function(cardId) {
 
 if (cardsInPlay.length === 2) {
 	alert("You found a match!");
-} else if (cardsInPlay[0] === cardsInPlay[2]) {
+} else if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("Sorry, try again.");
 };
 
